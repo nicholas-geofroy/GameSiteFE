9c05528e9afd663c61c68682491b3b511519fb3d
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-09-22 22:48:58 -0400

    add ability to have custom named lobbies

--- src/components/lobby/new-lobby.js
+++ src/components/lobby/new-lobby.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import { Redirect } from "react-router-dom";
+import "./lobby.css";
 
 import { http_url, default_opts, useApi } from "../../backend";
 
-const useCreateLobby = () => {
+const useCreateLobby = (id) => {
+  console.log("useCreateLobby: ", id);
   const opts = { ...default_opts, method: "POST", body: "{}" };
-  return useApi(http_url + "/lobby", opts);
+  return useApi(http_url + "/lobby/" + id, opts);
 };
 
-const NewLobby = () => {
-  const { loading, error, data } = useCreateLobby();
+const NewLobbyRequest = (params) => {
+  const { loading, error, data } = useCreateLobby(params.lobbyId);
 
   if (loading) {
     return <div>Creating Lobby ... </div>;
@@ -20,6 +23,49 @@ const NewLobby = () => {
 
   console.log(`created lobby with id ${data.id}`);
   return <Redirect to={`/lobby/${data.id}`} />;
+};
+
+const toValidLobbyId = (id) => {
+  return id.replaceAll(/[\W]/g, "_");
+};
+
+const NewLobby = () => {
+  const [lobbyId, setLobbyId] = useState("");
+  const [submit, setSubmit] = useState(false);
+  const onSubmit = (event) => {
+    setSubmit(true);
+    event.preventDefault();
+  };
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      onSubmit(event);
+      return false;
+    }
+  };
+
+  if (submit) {
+    return <NewLobbyRequest lobbyId={toValidLobbyId(lobbyId)} />;
+  }
+
+  return (
+    <form>
+      <h3 className="title">Create/Join a Lobby</h3>
+      <input
+        type="text"
+        id="lobbyNameInput"
+        className="surface"
+        placeholder="Lobby Name"
+        value={lobbyId}
+        onChange={(e) => setLobbyId(e.target.value)}
+        onKeyDown={onKeyDown}
+      ></input>
+      <button className="button primary" onClick={onSubmit} type="button">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default NewLobby;
